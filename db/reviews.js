2e6140f3f2cb49b9196c497e0a87d2c52201d436
Josef Berthoud <josef.berthoud@gmail.com>
2019-07-07 13:56:39 -0700

    seeding functional, missing auto increment id and children

--- db/reviews.js
+++ db/reviews.js
@@ -2,16 +2,25 @@
 
 const mongoose = require('mongoose');
 const db = require('./index.js');
+const autoIncrement = require('mongoose-auto-increment');
 var Schema = mongoose.Schema;
+require('mongoose-type-url');
 
+const Url = mongoose.SchemaTypes.Url;
+
+// autoIncrement.initialize(db);
+
+//currently schema not supporting child comments
+//ID auto-incrementing to allow for references from
+//child collection reviews at later date
 const reviewSchema = new Schema({
-  reviewID: Number,
   userName: String,
   textBody: String,
   datePosted: {
     type: Date,
     default: Date.now,
   },
+  avatar: Url,
   ratings: {
     accuracy: Number,
     communication: Number,
@@ -20,10 +29,9 @@ const reviewSchema = new Schema({
     checkIn: Number,
     value: Number,
   },
-  childComments: [{
-    id: Number,
-  }],
 });
+
+// reviewSchema.plugin(autoIncrement.plugin, 'Review');
 
 const Review = mongoose.model('Review', reviewSchema);
 
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,41 @@
-// seed DB with sampledata
+
+const db = require('./index.js');
+const Review = require('./reviews.js');
+const Faker = require('faker');
+
+//evenly represent ratings 1-5 stars
+const getRandomRatingInt = () => {
+  min = Math.ceil(1);
+  max = Math.floor(5);
+  return Math.floor(Math.random() * (max - min + 1)) + min; 
+};
+
+var fakeDocs = [];
+for (let i = 0; i < 100; i ++) { 
+  let fakeDoc = {
+    userName: Faker.fake("{{name.firstName}}, {{name.lastName}}"),
+    textBody: Faker.lorem.paragraph(),
+    datePosted: Date.now(),
+    avatar: "https://picsum.photos/48/48",
+    ratings: {
+      accuracy: getRandomRatingInt(),
+      communication: getRandomRatingInt(),
+      cleanliness: getRandomRatingInt(),
+      location: getRandomRatingInt(),
+      checkIn: getRandomRatingInt(),
+      value: getRandomRatingInt(),
+    },
+  };
+  fakeDocs.push(fakeDoc);
+}
+
+Review.insertMany(fakeDocs, (err, docs) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('woo! we added docs!');
+  }
+});
+
+
+
