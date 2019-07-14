f3eb4627d355843359a99b4d9e24b6797cfbcc8d
Josef Berthoud <josef.berthoud@gmail.com>
2019-07-13 18:07:27 -0700

    basic styling complete

--- client/src/components/PageCarousel.jsx
+++ client/src/components/PageCarousel.jsx
@@ -13,12 +13,6 @@ import {
   ArrowButton,
 } from './styleComponents.jsx';
 
-//this component needs to render between 6 and 9 divs in a carousel below reviews list, depending on review data
-//at any given time, between 1 and 2 of those divs will be uninteractable "..." divs
-//at any given time, between 1 and 2 of the divs will be next / last arrow buttons on the edges
-//at 6 (i.e. the first or last page) only one arrow button will be rendered (move up if at first page, down at last)
-//at 7, 8, or 9 divs rendered, both arrow buttons are rendered
-//
 
 class PageCarousel extends React.Component {
   constructor(props) {
@@ -27,7 +21,6 @@ class PageCarousel extends React.Component {
     this.state = {};
   }
 
-  //render will call getNumberOfDivs and map across that area, passing in prop types and values as needed
   render() {
     let { page, numberOfPages, changePage } = this.props;
     let n = numberOfPages;
