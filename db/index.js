3584205feb626f03bd211218283b684e1971769e
Josef Berthoud <josef.berthoud@gmail.com>
2019-07-18 18:55:03 -0700

    Edit logging

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://172.17.0.3:27017/reviews');
+mongoose.connect('mongodb://172.17.0.2:27017/reviews');
 
 module.exports.mongoose = mongoose;
